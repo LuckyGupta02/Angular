9ac6829b3336a74973cb63148e7bcfd2b6578bc5
Lucky Gupta <109872023+LuckyGupta02@users.noreply.github.com>
2023-11-23 17:07:42 +0000

    filter file

--- myproject/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { Route } from '@angular/router';
-
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
